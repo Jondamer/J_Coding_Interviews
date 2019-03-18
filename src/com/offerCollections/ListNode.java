8b94a656cf065499c9302449d99bb9ccee65841f
jondam <jondamer@gmail.com>
2019-03-18 13:55:39 +0800

    <--- add ListNode class --->

---
+++ src/com/offerCollections/ListNode.java
@@ -0,0 +1,16 @@
+package com.offerCollections;
+
+public class ListNode {
+
+	public ListNode() {
+		// TODO Auto-generated constructor stub
+	}
+
+ public   int val;
+ public   ListNode next = null;
+
+   public ListNode(int val) {
+       this.val = val;
+   }
+  
+}
